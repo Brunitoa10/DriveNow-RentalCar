307b1fdf299d80f02d73f618e1020825b5b703c7
Bruno A Parisi <bnoprs00@gmail.com>
2024-12-15 19:08:37 -0300

    Funcionalidad a boton delete

--- app/(routes)/(dashboard)/dashboard/admin/cars-manager/components/ListCars/CardCar/CardCar.tsx
+++ app/(routes)/(dashboard)/dashboard/admin/cars-manager/components/ListCars/CardCar/CardCar.tsx
@@ -2,14 +2,33 @@
 
 
 import { Button } from "@/components/ui/button";
+import { toast } from "@/hooks/use-toast";
+import axios from "axios";
 import { Fuel, Gauge, Gem, Trash, Upload, Users, Wrench } from "lucide-react";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 import { ButtonEditCar } from "./ButtonEditCar";
 import { CardCarProps } from "./CardCar.types";
 
 
 export function CardCar(props: CardCarProps) {
   const { car } = props;
+  const router = useRouter();
+  const deleteCar = async () => {
+    try{
+      await axios.delete(`/api/car/${car.id}`);
+      toast({
+        title: "Car deleted",
+      })
+      router.refresh();
+    }catch(error){
+      console.log(error);
+      toast({
+        title: "Something went wrong",
+        variant: "destructive"
+      });
+    }
+  };
     return (
     <div className="relative p-1 bg-white rounded-lg shadow-md hover:shadow-lg">
         <Image src={car.photo} alt={car.name} width={400} height={600} className="rounded-lg"/>
@@ -53,7 +72,7 @@ export function CardCar(props: CardCarProps) {
               <ButtonEditCar carData={car}/>
           </div>
           {car.isPublish ? (
-            <Button className="w-full mt-3" variant="outline" onClick={() => console.log("Unpublish")}>
+            <Button className="w-full mt-3" variant="outline" onClick={deleteCar}>
               Unpublish
               <Upload className="w-4 h-4 ml-2"/>
             </Button>
