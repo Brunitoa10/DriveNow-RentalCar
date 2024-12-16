121d8f8368e2f0b6e5e29b1949f641dad45c8f21
Bruno A Parisi <bnoprs00@gmail.com>
2024-12-16 01:35:18 -0300

    Admin managers-car lista

--- app/(routes)/(dashboard)/dashboard/admin/cars-manager/components/ListCars/CardCar/CardCar.tsx
+++ app/(routes)/(dashboard)/dashboard/admin/cars-manager/components/ListCars/CardCar/CardCar.tsx
@@ -14,9 +14,31 @@ import { CardCarProps } from "./CardCar.types";
 export function CardCar(props: CardCarProps) {
   const { car } = props;
   const router = useRouter();
+  const handlerPublishCar = async (publish: boolean) => {
+    try {
+      await axios.patch(`/api/car/${car.id}/form`,  { isPublish:publish });
+      if(publish){
+        toast({
+          title: "Car Published",
+         
+        })
+      }else{
+        toast({
+          title: "Car unPublish"
+        })
+      }
+     router.refresh();
+    } catch (error) {
+      console.log(error);
+      toast({
+        title: "Something went wrong",
+        variant: "destructive"
+      })
+    }
+  }
   const deleteCar = async () => {
     try{
-      await axios.delete(`/api/car/${car.id}`);
+      await axios.delete(`/api/car/${car.id}/form`);
       toast({
         title: "Car deleted",
       })
@@ -33,9 +55,9 @@ export function CardCar(props: CardCarProps) {
     <div className="relative p-1 bg-white rounded-lg shadow-md hover:shadow-lg">
         <Image src={car.photo} alt={car.name} width={400} height={600} className="rounded-lg"/>
         {car.isPublish ? (
-          <p className="absolute top-0 right-0 w-full p-1 text-center text-white bg-green-700">Published</p>
+          <p className="absolute top-0 right-0 w-full p-1 text-center text-white bg-green-700 rounded-t-lg">Published</p>
         ) : (
-          <p className="absolute top-0 left-0 right-0 w-full p-1 text-center text-white bg-red-300">Not Published</p>
+          <p className="absolute top-0 left-0 right-0 w-full p-1 text-center text-white bg-red-300 rounded-t-lg">Not Published</p>
         )}
         <div className="relative p-3">
           <div className="flex flex-col mb-3 gap-x-4">
@@ -65,19 +87,19 @@ export function CardCar(props: CardCarProps) {
             </p>
           </div>
           <div className="flex justify-between mt-3 gap-x-4">
-              <Button variant="outline" onClick={() => console.log("delete")}>
+              <Button variant="outline" onClick={deleteCar}>
                 Delete
                 <Trash className="w-4 h-4 ml-2"/>
               </Button>
               <ButtonEditCar carData={car}/>
           </div>
           {car.isPublish ? (
-            <Button className="w-full mt-3" variant="outline" onClick={deleteCar}>
+            <Button className="w-full mt-3" variant="outline" onClick={() => handlerPublishCar(false)}>
               Unpublish
               <Upload className="w-4 h-4 ml-2"/>
             </Button>
           ) : (
-            <Button className="w-full mt-3" variant="outline" onClick={() => console.log("Publish")}>
+            <Button className="w-full mt-3" variant="outline" onClick={() => handlerPublishCar(true)}>
               Publish
               <Upload className="w-4 h-4 ml-2"/>
             </Button>
